41d2c82d5dd41cbf66d6b30a29e72677bae9cf26
yeremiadio <raikkonendio@gmail.com>
2024-06-19 06:59:30 +0700

    feat: add module resolver for src alias absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          '@/api': './src/api',
+          '@/assets': './src/assets',
+          '@/components': './src/components',
+          '@/constants': './src/constants',
+          '@/utils': './src/utils',
+          '@/screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
