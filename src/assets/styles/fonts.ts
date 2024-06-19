433989c7c91cc4a0aec118b5bf77f2ca85a9c0ed
yeremiadio <raikkonendio@gmail.com>
2024-06-19 07:00:07 +0700

    feat: export fonts objects

---
+++ src/assets/styles/fonts.ts
@@ -0,0 +1,10 @@
+const Fonts = {
+  InterRegular: 'Inter-Regular',
+  InterMedium: 'Inter-Medium',
+  InterSemiBold: 'Inter-SemiBold',
+  InterThin: 'Inter-Thin',
+  InterBold: 'Inter-Bold',
+  InterBlack: 'Inter-Black',
+};
+
+export default Fonts;
