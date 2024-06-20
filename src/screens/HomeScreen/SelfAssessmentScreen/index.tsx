431f3f8157baeceffa82f862a2da65d9ac0b9be7
yeremiadio <raikkonendio@gmail.com>
2024-06-20 09:19:13 +0700

    feat: add react native paper button with fixed bottom

--- src/screens/HomeScreen/SelfAssessmentScreen/index.tsx
+++ src/screens/HomeScreen/SelfAssessmentScreen/index.tsx
@@ -1,16 +1,26 @@
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import Fonts from '@/assets/styles/fonts';
 import {RootStackParamList} from '@/types/reactNavigation';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {Button} from 'react-native-paper';
+import FixedBottom from '@/components/FixedBottom';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'SelfAssessment'>;
 
-const SelfAssessmentScreen = ({navigation}: Props) => {
+const SelfAssessmentScreen = (_: Props) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>SelfAssessmentScreen</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <FixedBottom>
+        <Button
+          onPress={() => {}}
+          mode="contained"
+          theme={{roundness: 10}}
+          contentStyle={styles.buttonFooter}>
+          Self Assessment
+        </Button>
+      </FixedBottom>
     </View>
   );
 };
@@ -21,6 +31,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  buttonFooter: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    // flex: 1,
   },
   text: {
     textAlign: 'center',
