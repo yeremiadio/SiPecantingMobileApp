1db47833afc2288b7bbc7e65860e0266f3cf6042
yeremiadio <raikkonendio@gmail.com>
2024-06-24 17:47:00 +0700

    feat: add new eslint react rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,8 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
+    'react-native/no-inline-styles': 0,
+    'react/no-unstable-nested-components': 0,
+    'react-hooks/exhaustive-deps': 0, // <--- THIS IS THE NEW RULE
   },
 };
