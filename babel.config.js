fea7ac24b40f2862b0d88e8b056fc684bb08dc4e
yeremiadio <raikkonendio@gmail.com>
2024-07-21 12:55:59 +0700

    feat: add several libraries axios, and async storage

--- babel.config.js
+++ babel.config.js
@@ -23,5 +23,18 @@ module.exports = {
       },
     ],
     'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: true,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
   ],
 };
