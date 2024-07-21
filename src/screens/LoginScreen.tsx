70c9812be6d5868c70833afdc3d8db5f98f9d547
yeremiadio <raikkonendio@gmail.com>
2024-07-21 08:35:20 +0700

    feat: revamp login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import Fonts from '@/assets/styles/fonts';
@@ -8,7 +8,9 @@ import {useAppDispatch} from '@/store';
 import {signIn} from '@/store/slices/authSlice';
 
 import {RootStackParamList} from '@/types/reactNavigation';
-
+import {Button, Text} from 'react-native-paper';
+import InputText from '@/components/inputs/InputText';
+import IllustationMotherSvg from '@/assets/svg/Motherhood-amico.svg';
 type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
 
 const LoginScreen = (_: Props) => {
@@ -17,20 +19,53 @@ const LoginScreen = (_: Props) => {
     dispatch(signIn());
   };
   return (
-    <SafeAreaView>
-      <View style={styles.loginContainer}>
-        <Text style={styles.text}>Login Page</Text>
-        <Button onPress={handleLogin} title="Login" />
-      </View>
-    </SafeAreaView>
+    <View style={styles.loginContainer}>
+      <ScrollView
+        contentContainerStyle={{justifyContent: 'center'}}
+        style={styles.formLoginContainer}>
+        <IllustationMotherSvg
+          width={240}
+          height={240}
+          style={{alignSelf: 'center', marginBottom: 8, marginTop: 36}}
+        />
+        <Text variant="headlineLarge">Welcome back</Text>
+        <Text variant="bodyLarge">
+          We're happy to see you again. Please enter the correct credential
+          information
+        </Text>
+        <View style={{marginTop: 8}}>
+          <InputText
+            label="Email"
+            mode="outlined"
+            placeholder="Please enter your email..."
+          />
+          <InputText
+            label="Password"
+            mode="outlined"
+            secureTextEntry
+            placeholder="Please enter your password..."
+          />
+        </View>
+        <Button
+          mode="contained"
+          onPress={handleLogin}
+          style={{marginTop: 16}}
+          contentStyle={{height: 48}}
+          theme={{roundness: 10}}>
+          Log in
+        </Button>
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   loginContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+    padding: 16,
+  },
+  formLoginContainer: {
+    maxHeight: '100%',
   },
   text: {
     textAlign: 'center',
